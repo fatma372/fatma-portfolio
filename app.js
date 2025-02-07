5a2588e003f5fae5e6c9b0ca17ae0b30d85c552b
fatma372 <fa1640@fayoum.edu.eg>
2025-02-07 17:30:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+let projectsContainer = document.querySelector(".projects-container");
+const projects = [
+    {
+        title: "Cartoons",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/cartoons.png",
+        link: "https://fatma372.github.io/Cartoons/",
+        code: "https://github.com/fatma372/Cartoons.git"
+    },
+    {
+        title: "Bakery",
+        technologies: ["HTML", "CSS","Bootstrap", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/bakery.png",
+        link: "https://fatma372.github.io/bakery/",
+        code: "https://github.com/fatma372/bakery.git"
+    },
+    {
+        title: "Travilifa",
+        technologies: ["HTML", "CSS", "JavaScript", "React","Bootstrap"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/travilifa.png",
+        link: "https://travilifa.vercel.app/",
+        code: "https://github.com/fatma372/Travilifa.git"
+    },
+    {
+        title: "Shopify",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/shopify.png",
+        link: "https://fatma372.github.io/shopify/",
+        code: "https://github.com/fatma372/shopify.git"
+    },
+    {
+        title: "Get-shop",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/get.png",
+        link: "https://fatma372.github.io/Get-shop/",
+        code: "https://github.com/fatma372/Get-shop.git"
+    },
+    {
+        title: "Postat",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/postat.png",
+        link: "https://fatma372.github.io/Postat/",
+        code: "https://github.com/fatma372/Postat.git"
+    },
+    {
+        title: "To Do",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        image: "images/projects/todo.png",
+        link: "https://fatma372.github.io/to-do-list/",
+        code: "https://github.com/fatma372/to-do-list.git"
+    },
+    
+]
+
+projects.forEach(project => {
+    projectsContainer.innerHTML += `
+    <div class="project-card card col-lg-3 col-8 p-2">
+                <img class="card-img" src=${project.image} alt=${project.title}>
+               <div class="card-body">
+                <h5 class="card-title">${project.title}</h5>
+                <p class="languages">
+                   ${project.technologies.join(", ")}.
+                </p>
+                
+                <p class="card-text description">${project.description}</p>
+                <a href=${project.link} target="_blank" class="link btn btn-dark text-dark shadow-sm"><i class="fa-solid fa-link"></i> <span class="text-light"> view </span> </a>
+                <a href=${project.code} target="_blank" class="code btn btn-dark text-dark shadow-sm"><i class="fab fa-github"></i> </a>
+                
+               </div>
+            </div>
+    `;
+})
